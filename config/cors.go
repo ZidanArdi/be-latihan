c85294b66f7ca00e688428a398c1d7c553c7ad38
ZidanArdi <panjihadi6@gmail.com>
2026-06-22 15:40:48 +0700

    push

--- config/cors.go
+++ config/cors.go
@@ -2,7 +2,8 @@ package config
 
 var allowedOrigins = []string{
 	"http://localhost:5173",
-	//"http://be-latihan-production-8f9c.up.railway.app"
+	"https://be-latihan-production-8f9c.up.railway.app",
+	"https://my-fe-omega.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
